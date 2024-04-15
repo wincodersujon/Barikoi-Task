96d532669eab2c1cd6b007c2e7aef334b5c6ac9c
sujon biswas <sujonbiswas2391@gmail.com>
2024-04-16 00:25:51 +0600

    I have created an API(userdata) where the endpoints are JWT-protected.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\JWTToken;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function UserList(Request $request){
+        try{
+            $users = User::orderBy('id', 'desc')->select('id', 'name','email')->paginate($request->per_page);
+            $users->appends($request->all());
+            $data['users'] = $users;
+            return response()->json([
+                'success' => true,
+                'message' => 'User List',
+                'data' => $data,
+            ], 200);
+        }
+        catch (\Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 200);
+        }
+    }
+    function UserRegistration(Request $request){
+        try {
+            User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => $request->input('password'),
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successfully'
+            ],200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'User Registration Failed'
+            ],200);
+
+        }
+    }
+
+    function UserLogin(Request $request){
+       $count=User::where('email','=',$request->input('email'))
+            ->where('password','=',$request->input('password'))
+            ->select('id')->first();
+
+       if($count!==null){
+           // User Login-> JWT Token Issue
+           $token=JWTToken::CreateToken($request->input('email'),$count->id);
+           return response()->json([
+               'status' => 'success',
+               'message' => 'User Login Successfully',
+                //'token' => $token /*If you want to show token when you are logged in */
+
+            ],200)->cookie('token',$token,time()+60*24*30); /*If you are use token set in cookie then you can use it and with time duration.*/
+       }
+       else{
+           return response()->json([
+               'status' => 'failed',
+               'message' => 'unauthorized'
+           ],401);
+
+       }
+
+    }
+
+    function UserLogout(){
+        return redirect('/')->cookie('token','',-1);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\TokenVerificationMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/user-registration',[UserController::class,'UserRegistration']);
+Route::post('/user-login',[UserController::class,'UserLogin']);
+Route::get('/user-list',[UserController::class,'UserList'])->middleware([TokenVerificationMiddleware::class]);
